bef7ca6a6e777b5525a3a00ccb5e6a1794b219b0
hildelio <hildelio@gmail.com>
2024-04-02 13:58:00 -0300

    feat: ajustes no css

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+  copyRight = '© 2024 All rights reserved';
+
+}
